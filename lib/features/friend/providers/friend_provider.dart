1d8fba5fae18f459c5c24bd4dfa8a51fd7351379
chukynya <henry.christian7557@gmail.com>
2026-01-20 08:16:35 +0700

    fix: removing unused import

--- lib/features/friend/providers/friend_provider.dart
+++ lib/features/friend/providers/friend_provider.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:studysphere_app/shared/models/user_model.dart';
 import 'package:studysphere_app/features/friend/services/friend_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class FriendProvider extends ChangeNotifier {
   final FriendService _friendService = FriendService();
